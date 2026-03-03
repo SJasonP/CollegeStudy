54959b017ff044738b1233f68178894c0e9563b1
SJasonP <SJasonP@iCloud.com>
2026-03-03 10:53:00 +0800

    Practice 0.1

---
+++ 0.0.helloworld.py
@@ -0,0 +1,4 @@
+# Author: SJasonP <SJasonP@iCloud.com>
+# Repo: https://github.com/SJasonP/CollegeStudy
+
+print("Hello World!")
---
+++ 0.1.vars.py
@@ -0,0 +1,7 @@
+# Author: SJasonP <SJasonP@iCloud.com>
+# Repo: https://github.com/SJasonP/CollegeStudy
+
+message = "Python is the best programming language, right? Definitely not PHP!"
+print()
+print("YOU RECEIVED A NEW MESSAGE: ")
+print("    " + message)
