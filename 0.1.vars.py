befbfaed74dd452289b55281ec5613d70d39fea3
SJasonP <SJasonP@iCloud.com>
2026-03-03 10:59:33 +0800

    Update Practice 0.1

--- 0.1.vars.py
+++ 0.1.vars.py
@@ -1,7 +1,14 @@
 # Author: SJasonP <SJasonP@iCloud.com>
 # Repo: https://github.com/SJasonP/CollegeStudy
 
-message = "Python is the best programming language, right? Definitely not PHP!"
+message = "Python is the best programming language, right?"
+
+print()
+print("YOU RECEIVED A NEW MESSAGE: ")
+print("    " + message)
+
+message = "Why not PHP !?"
+
 print()
 print("YOU RECEIVED A NEW MESSAGE: ")
 print("    " + message)
