412b6f13e714bde8e7f962ba4db3f0cb71375e54
SJasonP <SJasonP@iCloud.com>
2026-03-05 10:25:38 +0800

    Python.0.2-8
    Just some practice.

---
+++ Python/0/0.8.py
@@ -0,0 +1,8 @@
+# Author: SJasonP <SJasonP@iCloud.com>
+# Repo: https://github.com/SJasonP/CollegeStudy
+
+number = input("请输入数字：")
+print(number)
+print(type(number))
+# print(number + 10)
+print(int(number) + 10)
