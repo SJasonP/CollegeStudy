94760a23041edb0c176d2e3a70fedbb564e9a6c4
SJasonP <SJasonP@iCloud.com>
2026-03-10 10:41:37 +0800

    Update Python.1

---
+++ Python/1/1.0.numbers.py
@@ -0,0 +1,10 @@
+# Author: SJasonP <SJasonP@iCloud.com>
+# Repo: https://github.com/SJasonP/CollegeStudy
+
+print(7 + 1)
+print(10 - 2)
+print(2 * 4)
+print(int(16208 / 2026))
+
+lucky_num = 7
+print("My favorite number is " + str(lucky_num) + "! No reason.")
